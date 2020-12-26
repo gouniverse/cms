f5c643f4337562bb2e855e788f00f3fc3926314f
Milan Lesichkov <lesichkovm@gmail.com>
2020-12-26 19:28:59 +0000

    Create cms.go

---
+++ cms.go
@@ -0,0 +1,31 @@
+package cms
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var dbInstance *gorm.DB
+var prefix string
+
+func Init(driverName string, dsn string) {
+	prefix = "cms_"
+	//sqlDB, err := sql.Open("mysql", "mydb_dsn")
+	// gormDB, err := gorm.Open(mysql.New(mysql.Config{
+	// 	Conn: sqlDB,
+	// }), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Entity{})
+	db.AutoMigrate(&EntityAttribute{})
+
+	dbInstance = db
+}
+
+func GetDb() *gorm.DB {
+	return dbInstance
+}
