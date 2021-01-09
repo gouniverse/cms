9f3d9cd507517ffd872d38e6a8c616a3ed1f8e60
Milan Lesichkov <lesichkovm@gmail.com>
2021-01-09 07:57:23 +0000

    Create entity_test.go

---
+++ entity_test.go
@@ -0,0 +1,28 @@
+package cms
+
+import (
+	"testing"
+	//"database/sql"
+   _ "github.com/mattn/go-sqlite3"
+  "gorm.io/driver/sqlite"
+  "gorm.io/gorm"
+)
+
+func InitDB(filepath string) *gorm.DB /**sql.DB*/ {
+
+  db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+  if err != nil { panic(err) }
+  // Auto Migrate
+  db.AutoMigrate(&Entity{})
+	// db, err := sql.Open("sqlite3", filepath)
+	// if err != nil { panic(err) }
+	// if db == nil { panic("db nil") }
+	// return db
+	return db
+}
+func TestCreation(t *testing.T) {
+	InitDB("entity_test.db")
+	//if sql != "SELECT * FROM 'user';" {
+		//t.Fatalf(sql)
+	//}
+}
