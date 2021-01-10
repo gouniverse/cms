bc3035719aa54dd4d61623e4eea7cf6c049f0c53
Milan Lesichkov <lesichkovm@gmail.com>
2021-01-10 05:40:47 +0000

    Update admin_template_routes.go

--- admin_template_routes.go
+++ admin_template_routes.go
@@ -59,7 +59,7 @@ func pageTemplateTemplateManager(w http.ResponseWriter, r *http.Request) {
 	modalContent := hb.NewDiv().Attr("class", "modal-content")
 	modalHeader := hb.NewDiv().Attr("class", "modal-header").AddChild(hb.NewHeading5().HTML("New Template"))
 	modalBody := hb.NewDiv().Attr("class", "modal-body")
-	modalBody.AddChild(hb.NewDiv().Attr("class", "form-group").AddChild(hb.NewLabel().HTML("Title")).AddChild(hb.NewInput().Attr("class", "form-control").Attr("v-model", "templateCreateModel.title")))
+	modalBody.AddChild(hb.NewDiv().Attr("class", "form-group").AddChild(hb.NewLabel().HTML("Name")).AddChild(hb.NewInput().Attr("class", "form-control").Attr("v-model", "templateCreateModel.name")))
 	modalFooter := hb.NewDiv().Attr("class", "modal-footer")
 	modalFooter.AddChild(hb.NewButton().HTML("Close").Attr("class", "btn btn-prsecondary").Attr("data-bs-dismiss", "modal"))
 	modalFooter.AddChild(hb.NewButton().HTML("Create & Continue").Attr("class", "btn btn-primary").Attr("v-on:click", "templateCreate"))
@@ -104,7 +104,7 @@ const TemplateManager = {
 	data() {
 		return {
 		  templateCreateModel:{
-			  title:"Test"
+			  name:""
 		  }
 		}
 	},
@@ -115,12 +115,11 @@ const TemplateManager = {
 			modalTemplateCreate.show();
 		},
 		templateCreate(){
-			var title = this.templateCreateModel.title;
-		    $.post(templateCreateUrl, {title: title}).done((result)=>{
+			var name = this.templateCreateModel.name;
+			$.post(templateCreateUrl, {name: name}).done((result)=>{
 				if (result.status==="success"){
 					var modalTemplateCreate = new bootstrap.Modal(document.getElementById('ModalTemplateCreate'));
-			        modalTemplateCreate.hide();
-
+				        modalTemplateCreate.hide();
 					return location.href = templateUpdateUrl+ "&template_id=" + result.data.template_id;
 				}
 				alert("Failed. " + result.message)
