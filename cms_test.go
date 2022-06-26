d5e0bcb0ce35c40fda310fc666e28b819111c471
Milan Lesichkov <lesichkovm@gmail.com>
2022-06-26 07:14:08 +0000

    Fixed tests

--- cms_test.go
+++ cms_test.go
@@ -1,7 +1,6 @@
 package cms
 
 import (
-	"log"
 	"testing"
 
 	"database/sql"
@@ -29,6 +28,10 @@ func init() {
 	// }
 }
 
+func TestCmsTestSuite(t *testing.T) {
+	suite.Run(t, new(CmsTestSuite))
+}
+
 type CmsTestSuite struct {
 	suite.Suite
 	VariableThatShouldStartAtFive int
@@ -37,42 +40,80 @@ type CmsTestSuite struct {
 // Make sure that VariableThatShouldStartAtFive is set to five
 // before each test
 func (suite *CmsTestSuite) SetupTest() {
-	suite.VariableThatShouldStartAtFive = 5
+	// suite.VariableThatShouldStartAtFive = 5
 
 	// Setup()
 }
 
 // All methods that begin with "Test" are run as tests within a
 // suite.
-func (suite *CmsTestSuite) TestExample() {
-	assert.Equal(suite.T(), 5, suite.VariableThatShouldStartAtFive)
-}
+// func (suite *CmsTestSuite) TestExample() {
+// 	assert.Equal(suite.T(), 5, suite.VariableThatShouldStartAtFive)
+// }
 
 //TestAuth tests the auth page
 func (suite *CmsTestSuite) TestCmsInit() {
-	_, err := mainDb("sqlite", "", "", "./test_init.db", "", "")
-	assert.Nil(suite.T(), err, "DB error %s", err.Error())
-	// Init(Config{
-	// 	DbInstance:      db,
-	// 	EnableTemplates: true,
-	// 	EnablePages:     true,
-	// 	EnableBlocks:    true,
-	// 	EnableSettings:  true,
-	// 	// CustomEntityList: entityList(),
-	// })
+	db, err := mainDb("sqlite", "", "", "test_init.db", "", "")
+	assert.Nil(suite.T(), err, "DB error")
+	defer db.Close()
+
+	assert.False(suite.T(), configuration.EnableBlocks, "Enable blocks MUST BE false before init")
+	assert.False(suite.T(), configuration.EnableCache, "Enable cache MUST BE false before init")
+	assert.False(suite.T(), configuration.EnablePages, "Enable pages MUST BE false before init")
+	assert.False(suite.T(), configuration.EnableSettings, "Enable pages MUST BE false before init")
+	assert.False(suite.T(), configuration.EnableSession, "Enable pages MUST BE false before init")
+	assert.False(suite.T(), configuration.EnableTemplates, "Enable templates MUST BE false before init")
+
+	Init(Config{
+		DbInstance:      db,
+		EnableCache:     true,
+		EnablePages:     true,
+		EnableBlocks:    true,
+		EnableSettings:  true,
+		EnableSession:   true,
+		EnableTemplates: true,
+		// CustomEntityList: entityList(),
+	})
+
+	assert.True(suite.T(), configuration.EnableBlocks, "Enable blocks MUST BE true after init")
+	assert.True(suite.T(), configuration.EnableCache, "Enable cache MUST BE true after init")
+	assert.True(suite.T(), configuration.EnablePages, "Enable pages MUST BE true after init")
+	assert.True(suite.T(), configuration.EnableSettings, "Enable pages MUST BE true after init")
+	assert.True(suite.T(), configuration.EnableSession, "Enable pages MUST BE true after init")
+	assert.True(suite.T(), configuration.EnableTemplates, "Enable templates MUST BE true after init")
+
 	// pages, err := EntityStore.EntityList("page", 0, 10, "", "name", "ASC")
-	// assert.NotNil(suite.T(), err, "DB error %s", err.Error())
+	// assert.Nil(suite.T(), err, "Entity list MUST NOT throw errors")
 	// assert.Equal(suite.T(), 0, len(pages), "Pages must be 0 - %s found", len(pages))
-	//assert.True(suite.T(), cms.Config.EnableBlocks, "DB error %s", err.Error())
 	//assert.HTTPBodyContainsf(suite.T(), routes.Routes().ServeHTTP, "GET", "/auth", url.Values{}, "api key is required", "%")
 }
+
+// func (suite *CmsTestSuite) TestCmsPages() {
+// 	db, err := mainDb("sqlite", "", "", "test_pages.db", "", "")
+// 	assert.Nil(suite.T(), err, "DB error")
+// 	defer db.Close()
+
+// 	Init(Config{
+// 		DbInstance:      db,
+// 		EnableCache:     true,
+// 		EnablePages:     true,
+// 		EnableBlocks:    true,
+// 		EnableSettings:  true,
+// 		EnableSession:   true,
+// 		EnableTemplates: true,
+// 		// CustomEntityList: entityList(),
+// 	})
+
+// 	pages, err := EntityStore.EntityList("page", 0, 10, "", "name", "ASC")
+// 	assert.Nil(suite.T(), err, "Entity list MUST NOT throw errors")
+// 	assert.Equal(suite.T(), 0, len(pages), "Pages must be 0 - %s found", len(pages))
+// }
 
 func mainDb(driverName string, dbHost string, dbPort string, dbName string, dbUser string, dbPass string) (*sql.DB, error) {
 	var db *sql.DB
 	var err error
 	if driverName == "sqlite" {
 		dsn := dbName
-		log.Println(dsn)
 		db, err = sql.Open("sqlite", dsn)
 	}
 	if driverName == "mysql" {
@@ -89,10 +130,5 @@ func mainDb(driverName string, dbHost string, dbPort string, dbName string, dbUs
 	if db == nil {
 		return nil, errors.New("database for driver " + driverName + " could not be intialized")
 	}
-
 	return db, nil
-}
-
-func TestCmsTestSuite(t *testing.T) {
-	suite.Run(t, new(CmsTestSuite))
 }
